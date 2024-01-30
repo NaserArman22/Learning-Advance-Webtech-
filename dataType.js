594ecc6a94f9669d388d8f611dc31647ad191c59
NaserArman22 <157449191+NaserArman22@users.noreply.github.com>
2024-01-30 13:35:00 +0600

    js

---
+++ dataType.js
@@ -0,0 +1,9 @@
+let firstname="Naaer";
+let cgpa="3.75";
+let isOkay=true;
+let a=null;
+let z;
+let result=0/0;
+console.log(result);
+let notAnumber=parseInt("hi");
+console.log(notAnumber);
---
+++ variable.js
@@ -0,0 +1,5 @@
+var name="Arman";
+var name="Naser";
+let age=24;
+const PI=3.14;
+console.log(name);
